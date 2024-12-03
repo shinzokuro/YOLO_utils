7388ca84e15405a604bad9743c5e9d62c7438233
harsha_l <harsha_l@yupiteru.co.jp>
2024-12-03 15:14:47 +0900

    added custom savings

--- scripts/utils/yaml_utils.py
+++ scripts/utils/yaml_utils.py
@@ -7,20 +7,36 @@ def write_yaml(data, file_path):
         yaml.safe_dump(data, file, default_flow_style=False, sort_keys=False)
 
 
-def create_dataset_yamlfile(dataset_path, classes={0: "person"}, file_name=r"data"):
+def create_dataset_yamlfile(
+    dataset_path,
+    file_name=r"data",
+    train="images/train",
+    val="images/val",
+    test="images/val",
+    classes={0: "person"},
+):
     data = {
-        "path": rf"{dataset_path}",
-        "train": "images/train",  # train images (relative to 'path') 128 images
-        "val": "images/val",  # val images (relative to 'path') 128 images
-        "test": "images/val",  # test images (optional)
+        "path": dataset_path,
+        "train": train,  # train images (relative to 'path') 128 images
+        "val": val,  # val images (relative to 'path') 128 images
+        "test": test,  # test images (optional)
         "names": classes,
     }
     data_yaml_file = Path(dataset_path) / rf"{file_name}.yaml"
     write_yaml(data, data_yaml_file)
-
+    return data_yaml_file
 
 def read_yaml(file_path):
     data = None
     with open(Path(file_path), "r") as file:
         data = yaml.safe_load(file)
     return data
+
+
+def create_val_yaml(
+    dataset_path, new_val=r"images", new_test=r"images", file_name="test"
+):
+    # Read the YAML file
+    return create_dataset_yamlfile(
+        dataset_path, file_name=file_name, val=new_val, test=new_test
+    )
