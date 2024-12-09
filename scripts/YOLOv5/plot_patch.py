15058fa5cdbf2ecbd2dffbd3434d8b03e3b874cf
harsha_l <harsha_l@yupiteru.co.jp>
2024-12-09 16:41:37 +0900

    testing monkey patching 3

--- scripts/YOLOv5/plot_patch.py
+++ scripts/YOLOv5/plot_patch.py
@@ -1,10 +1,8 @@
 from pathlib import Path
-import pandas as pd
-import numpy as np
-import os
 from yolov5 import val
 from YOLO_utils.scripts.YOLOv5.config import create_val_config
 from YOLO_utils.scripts.utils.yaml_utils import create_val_yaml
+from YOLO_utils.scripts.YOLOv5.save_data import save_pr_curve_data, save_mc_curve_data
 import utils.metrics as metrics
 from utils.metrics import (
     plot_pr_curve as original_plot_pr_curve,
@@ -12,56 +10,6 @@ from utils.metrics import (
 )
 
 
-# AP, PR Curve
-def save_pr_curve_data(px, py, ap, save_dir="pr_curve.png", names=()):
-    """_summary_
-    saves the data in excel
-    """
-    # Convert px, py, ap to numpy arrays if they are not already
-    px = np.array(px)
-    py = np.array(py)
-    ap = np.array(ap)
-
-    # Flatten the results
-    precision = px.flatten()
-    recall = py.flatten()
-    ap_flat = ap.flatten()
-
-    # Get the save paths
-    save_path = os.path.dirname(save_dir)
-    pr_excel_path = os.path.join(save_path, "Precision-Recall_Curve.xlsx")
-    ap_excel_path = os.path.join(save_path, "IOU_AP.xlsx")
-
-    # Create their respective dataframes
-    ap_data = {"ap": ap_flat}
-    pr_data = {"Precision": precision, "Recall": recall}
-    pr_df, ap_df = pd.DataFrame(pr_data), pd.DataFrame(ap_data)
-
-    # Save the recall, precision, and ap data to Excel files
-    pr_df.to_excel(pr_excel_path, index=False)
-    ap_df.to_excel(ap_excel_path, index=False)
-
-
-# Confidence-F1 Curve, Confidence-Precision Curve, Confidence-Recall Curve
-def save_mc_curve_data(
-    px, py, save_dir="mc_curve.png", names=(), xlabel="Confidence", ylabel="Metric"
-):
-    """_summary_
-    saves the data in excel
-    """
-    px = np.array(px)
-    py = np.array(py)
-    # flatten the results
-    x_values = px.flatten()
-    y_values = py.flatten()
-    # get the save paths
-    path = os.path.dirname(save_dir)
-    path = os.path.join(path, rf"{xlabel}-{ylabel}_Curve.xlsx")
-    # create their respective dataframes
-    data = {rf"{xlabel}": x_values, rf"{ylabel}": y_values}
-    df = pd.DataFrame(data)
-    # Save the recall and precision data to an Excel file
-    df.to_excel(path, index=False)
 
 
 def modified_plot_pr_curve(px, py, ap, save_dir="pr_curve.png", names=()):
@@ -86,8 +34,6 @@ metrics.plot_mc_curve = modified_plot_mc_curve
 
 
 def run_validation(save_path: Path, test_data_path, pt_files: dict):
-    # metrics.plot_pr_curve = modified_plot_pr_curve
-    # metrics.plot_mc_curve = modified_plot_mc_curve
     test_data = [
         directory for directory in Path(test_data_path).iterdir() if directory.is_dir()
     ]
